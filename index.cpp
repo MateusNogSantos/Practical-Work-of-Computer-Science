0f9be916077b23e5552d5678b0611d88e897fcb8
Your Name <brunocustodio.inf@gmail.com>
2025-05-13 21:43:59 -0300

    v1.0.0

--- index.cpp
+++ index.cpp
@@ -2,18 +2,19 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
 int main(){
 
     int id;
-    string lixostr,nome;
+    string lixoStr,nome;
     ifstream entrada("lollapalufla.csv");
 
-    getline(entrada,lixostr);
+    getline(entrada,lixoStr); //Passa a primeira Linha do CSV para lixoStr
 
-    entrada >> id;
-    entrada.ignore(2);
-    getline(entrada,nome,'"');
-    cout << nome;
+    entrada >> id; //Lê do CSV o valor #ID da Banda para a variável id
+    entrada.ignore(2); //Desconsidera a primeira , e a primeira "
+    getline(entrada,nome,'"'); //Lê do CSV o valor do Nome da Banda para a variável nome 
+    cout << nome; //Printa o valor de nome na tela
     
     return 0;
 }
